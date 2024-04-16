159176a1a27b9e99c35db12a40adfe1cf01df88d
russellandrady <russellsuper2000@gmail.com>
2024-04-16 14:18:40 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async(req, res, next) => {
     const { username, email, password } = req.body;
@@ -11,5 +12,30 @@ export const register = async(req, res, next) => {
     }
     catch(err){
         next(err);
+    }
+}
+export const login = async(req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email }); //with findone we can find the first user with the email
+      if (!validUser) {
+        return next(errorHandler(404, "User not found"));
+      }
+      const validPassword = await bcryptjs.compare(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(401, "Wrong credentials"));
+      }
+      const { password: pass, ...rest } = validUser._doc; //we destructure the password from the user object and the rest. This _doc remove the unneccesary things
+      const token = jwt.sign(
+        { id: validUser._id, email: validUser.email },
+        process.env.JWT_SECRET
+      );
+      let expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour from now
+      res
+        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest); //we create a cookie named access_token with the token in client's browser that can only access by http. Not by third party apps. And finally we send the validUser credentials to the client
+    } catch (error) {
+      next(error);
     }
 }

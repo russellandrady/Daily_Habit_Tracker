694dc23fa0a44cf58a21a58e57c7c0a41530952e
russellandrady <russellsuper2000@gmail.com>
2024-04-20 21:04:15 +0530

    update habit route implemented

--- api/controllers/habit.controller.js
+++ api/controllers/habit.controller.js
@@ -1,4 +1,5 @@
 import Habit from "../models/habit.model.js";
+import {errorHandler} from "../utils/error.js";
 
 export const createHabit = async (req, res, next) => {
     const { habit, description } = req.body;
@@ -22,3 +23,31 @@ export const getAllHabits = async (req, res, next) => {
         next(err);
     }
 };
+export const updateHabit = async (req, res, next) => {
+
+    try {
+      const updatedHabit = await Habit.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            habit: req.body.habit,
+            description: req.body.description,
+            day1: req.body.day1,
+            day2: req.body.day2,
+            day3: req.body.day3,
+            day4: req.body.day4,
+            day5: req.body.day5,
+            day6: req.body.day6,
+            day7: req.body.day7,
+          },
+        },
+        { new: true } //for seeing the updated user in front end.
+      );
+      if(updatedHabit===null){
+        return next(errorHandler(404, "Habit not found"));
+      }
+      res.status(200).json(updatedHabit);
+    } catch (err) {
+      next(err);
+    }
+  };
--- api/routes/habit.route.js
+++ api/routes/habit.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createHabit, getAllHabits} from "../controllers/habit.controller.js";
+import { createHabit, getAllHabits, updateHabit} from "../controllers/habit.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createHabit);
 router.get("/all", verifyToken, getAllHabits);
+router.post("/update/:id", verifyToken, updateHabit);
 
 export default router;
